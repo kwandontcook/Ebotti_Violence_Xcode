ed0cb5c8b294ad84375f5dd9d1a583e9be34e923
Kwok Chung Kwan <kwandontcook@gmail.com>
2022-02-27 17:29:50 +1030

    Initial Commit

---
+++ Ebotti_Violence/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ebotti_Violence
+//
+//  Created by Mv Mobile on 27/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
